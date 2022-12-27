9f98f88f12bceea0ee9ba1189ddc277591032af3
Sime <sime.cvitan25@gmail.com>
2022-12-27 02:47:18 +0100

    changes

--- src/main/java/com/example/authservice/AuthServiceApplication.java
+++ src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -1,7 +1,14 @@
 package com.example.authservice;
 
+import com.example.authservice.domain.AppUser;
+import com.example.authservice.domain.Role;
+import com.example.authservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class AuthServiceApplication {
@@ -9,6 +16,27 @@ public class AuthServiceApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService){
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new AppUser(null, "Marko Markic", "marko", "1234", new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Nikola Tesla", "nikola", "abcd", new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Majstor Fantac", "fantac", "qwer", new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Bruce Wayne", "bruce", "xyz", new ArrayList<>()));
+
+			userService.addRoleToAppUser("marko", "ROLE_USER");
+			userService.addRoleToAppUser("fantac", "ROLE_USER");
+			userService.addRoleToAppUser("nikola", "ROLE_ADMIN");
+			userService.addRoleToAppUser("bruce", "ROLE_ADMIN");
+			userService.addRoleToAppUser("bruce", "ROLE_SUPER_ADMIN");
+
+		};
 	}
 
 }
--- src/main/java/com/example/authservice/api/UserController.java
+++ src/main/java/com/example/authservice/api/UserController.java
@@ -1,13 +1,13 @@
 package com.example.authservice.api;
 
 import com.example.authservice.domain.AppUser;
+import com.example.authservice.domain.Role;
 import com.example.authservice.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +18,33 @@ public class UserController {
     @Autowired
     private final UserService userService;
 
+    @PostMapping("/user/save")
+    public ResponseEntity<AppUser>saveUser(@RequestBody AppUser user){
+        return ResponseEntity.ok().body(userService.saveUser(user));
+    }
+
+    @PostMapping("/roles/save")
+    public ResponseEntity<Role>saveRole( @RequestBody Role role ){
+        return ResponseEntity.ok().body(userService.saveRole(role));
+    }
+
     @GetMapping("/users")
     public ResponseEntity<List<AppUser>>getUsers(){
         return ResponseEntity.ok().body(userService.getAppUsers());
     }
 
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToAppUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+
+
+}
+
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
